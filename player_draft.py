68c85ad3fb1ea4a6a5ad03cb6a9e020c2f44772a
cjy-2001 <106933110+cjy-2001@users.noreply.github.com>
2024-01-28 21:38:47 -0500

    Implement basic functionalities

--- player_draft.py
+++ player_draft.py
@@ -1,90 +1,11 @@
 from typing import Type
-import random
 
-# define globals for cards
-SUITS = ('C', 'S', 'H', 'D')
-RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K')
-VALUES = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10}
+from card import Card
+from hand import Hand
+from deck import Deck
+from config import VALUES
+
 THRESHOLD_WIN_RATE = 0.4
-
-# define card class
-class Card:
-    def __init__(self, suit, rank):
-        if (suit in SUITS) and (rank in RANKS):
-            self.suit = suit
-            self.rank = rank
-        else:
-            self.suit = None
-            self.rank = None
-            print(f'Invalid card: {suit} {rank}')
-
-    def __str__(self):
-        return self.suit + self.rank
-
-    def get_suit(self):
-        return self.suit
-
-    def get_rank(self):
-        return self.rank
-
-    def get_value(self):
-        return VALUES[self.rank]
-
-
-# define hand class
-class Hand:
-    def __init__(self):
-        self.cards = []
-
-    def __str__(self):
-        ans = "Hand contains "
-        for i in range(len(self.cards)):
-            ans += str(self.cards[i]) + " "
-        return ans
-        # return a string representation of a hand
-
-    def add_card(self, card):
-        self.cards.append(card)
-        # add a card object to a hand
-
-    def get_value(self):
-        value = 0
-        aces = False
-        for c in self.cards:
-            rank = c.get_rank()
-            v = VALUES[rank]
-            if rank == 'A': aces = True
-            value += v
-        if aces and value < 12: value += 10
-        return value
-        # count aces as 1, if the hand has an ace, then add 10 to hand value if it doesn't bust
-        # compute the value of the hand, see Blackjack video
-
-
-# define deck class
-class Deck:
-    def __init__(self):
-        self.deck = []
-        for s in SUITS:
-            for r in RANKS:
-                self.deck.append(Card(s, r))
-        # create a Deck object
-
-    def shuffle(self):
-        random.shuffle(self.deck)
-        # shuffle the deck
-
-    def deal_card(self):
-        return self.deck.pop()
-        # deal a card object from the deck
-
-    def __str__(self):
-        ans = "The deck: "
-        for c in self.deck:
-            ans += str(c) + " "
-        return ans
-        # return a string representing the deck
-    
 
 # define player class
 class Player:
@@ -95,10 +16,14 @@ class Player:
     def should_hit(self, player_value: int, dealer_face_value: int) -> bool:
         if player_value >= 21:
             return False
+        
+        if player_value <= 11:
+            return True 
+        
         # Check if there's enough data in the matrix to make a decision
-        if self.matrix[player_value][dealer_face_value]['counts'] > 500:
+        if self.matrix[player_value][dealer_face_value]['counts'] > 200:
             win_rate = self.matrix[player_value][dealer_face_value]['wins'] / self.matrix[player_value][dealer_face_value]['counts']
-            return win_rate >= 0.4
+            return win_rate >= THRESHOLD_WIN_RATE
         else:
             # Default strategy if not enough data
             return player_value < 17
@@ -138,10 +63,13 @@ class Player:
             result['counts'] += 1
 
             # Deal another card to player
+            if player_value == 21:
+                continue
+
             player_hand.add_card(deck.deal_card())
             player_value = player_hand.get_value()
 
-            if player_value >= 21:
+            if player_value > 21:
                 continue
             else:
                 outer_break = False
@@ -171,7 +99,7 @@ class Player:
         
         win_rate = self.matrix[player_value][dealer_face_value]['wins'] / self.matrix[player_value][dealer_face_value]['counts']
         
-        return win_rate >= 0.4
+        return win_rate >= THRESHOLD_WIN_RATE
         
 
     def play(self, trials: int) -> float:
@@ -188,8 +116,11 @@ class Player:
             player_hand.add_card(deck.deal_card())
             dealer_hand.add_card(deck.deal_card())
 
-            while self.hitme(player_hand, dealer_hand.cards[0]):
+            player_value = player_hand.get_value()
+
+            while player_value <= 11 or self.hitme(player_hand, dealer_hand.cards[0]):
                 player_hand.add_card(deck.deal_card())
+                player_value = player_hand.get_value()
 
             if player_hand.get_value() <= 21: 
                 self.dealer_turn(deck, dealer_hand)
@@ -222,10 +153,10 @@ class Player:
 def main():
     player = Player()
     
-    num_trials = 100000
+    num_trials = 5000000
     player.sim(num_trials)
     player.print_matrix()
-    print(player.play(1000))
+    print(player.play(10000))
     # win_rate = player.play(100)
     # print(f"Winning rate over 100 hands: {win_rate*100}%")
 
